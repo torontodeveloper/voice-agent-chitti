c71b8cea7e016388858ff1a56cb1e9e9f79aaa5c
Kevin Kakolla <nageswarkv@gmail.com>
2026-05-25 15:16:48 -0400

    port chaned

--- call_agent.sh
+++ call_agent.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python bot.py -t twilio -x chitti-voice-agent-hccffuccc7d7dfcf.canadaeast-01.azurewebsites.net --host 0.0.0.0 --port 8000
+python bot.py -t twilio -x chitti-voice-agent-hccffuccc7d7dfcf.canadaeast-01.azurewebsites.net --host 0.0.0.0 --port 7860
 
